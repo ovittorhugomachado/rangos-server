6afb12699b7270ead47ec001baa8cb4231d8c25c
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-06-11 13:11:48 -0300

    criado arquivo com erros padronizados

---
+++ src/utils/errors.ts
@@ -0,0 +1,26 @@
+export class AppError extends Error {
+    constructor(
+        public readonly message: string,
+        public readonly statusCode: number = 400
+    ) {
+        super(message);
+    }
+}
+
+export class NotFoundError extends AppError {
+    constructor(message: string) {
+        super(message, 404);
+    }
+}
+
+export class ValidationError extends AppError {
+    constructor(message: string) {
+        super(message, 422);
+    }
+}
+
+export class InternalServerError extends AppError {
+    constructor(message: string = 'Erro interno no servidor') {
+        super(message, 500);
+    }
+}
