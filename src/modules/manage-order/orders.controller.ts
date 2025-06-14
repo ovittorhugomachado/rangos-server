55fd87c58f005dbfbf93946aceb8789b6890f2d7
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-06-14 02:27:51 -0300

    criada rota de cancelar pedido

--- src/modules/manage-order/orders.controller.ts
+++ src/modules/manage-order/orders.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { NotFoundError } from "../../utils/errors";
-import { orderAcceptanceService } from "./orders.service";
+import { orderAcceptanceService, orderCancellationService } from "./orders.service";
 
 export const acceptOrder = async (req: Request, res: Response): Promise<void> => {
     try {
@@ -40,4 +40,44 @@ export const acceptOrder = async (req: Request, res: Response): Promise<void> =>
             });
         }
     }
-}
+};
+
+export const cancelOrder = async (req: Request, res: Response) => {
+
+    try {
+                const userId = Number(req.user?.userId);
+
+        if (!userId) {
+            res.status(401).json({ message: 'Erro na validação do usuário' });
+            return
+        };
+
+        const orderId = Number(req.params.orderId);
+
+        if (isNaN(orderId)) {
+            res.status(400).json({ message: 'ID do pedido inválido' });
+            return;
+        };
+
+        await orderCancellationService (userId, orderId);
+
+        res.status(200).json({ message: 'Pedido cancelado com sucesso' });
+        
+    } catch (error) {
+        
+        console.error('Erro ao cancelar pedido:', error);
+
+        if (error instanceof NotFoundError) {
+            res.status(404).json({
+                success: false,
+                message: error.message
+            });
+        }
+        else {
+            res.status(500).json({
+                success: false,
+                message: "Erro interno no servidor"
+            });
+        }
+    }
+};
--- src/modules/manage-order/orders.service.ts
+++ src/modules/manage-order/orders.service.ts
@@ -19,4 +19,24 @@ export const orderAcceptanceService = async (userId: number, orderId: number) =>
             status: 'em_preparo'
         }
     });
-}
+};
+
+export const orderCancellationService = async (userId: number, orderId: number) => {
+    
+    const store = await prisma.store.findUnique({ where: { userId } });
+    if (!store) throw new NotFoundError('Loja não encontrada');
+
+    const order = await prisma.order.findUnique({ where: { id: orderId } });
+    if (!order) throw new NotFoundError('Pedido não encontrado');
+
+    if (order.storeId !== store.id) {
+        throw new NotFoundError("Pedido não pertence a esta loja");
+    }
+
+    await prisma.order.update({
+        where: { id: orderId },
+        data: {
+            status: 'cancelado'
+        }
+    });
+};
