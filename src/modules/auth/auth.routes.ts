0f3d735acc38a7bbd976a7934e43b0c9e31809b0
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-06-15 23:40:49 -0300

    rotas de autenticação usando o asyncHandler

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,24 +1,17 @@
 import { Router } from 'express';
+import { asyncHandler } from '../../utils/async-handler';
 import { signUp, login, refreshAccessToken, logout } from './auth.controller';
 import { authLimiter } from './auth-limiter.middleware';
 import { authenticateToken } from '../../middlewares/authenticate-token.middleware';
 
 const router = Router();
 
-router.post('/signup', (req, res, next) => {
-    signUp(req, res).catch(next);
-});
+router.post('/signup', asyncHandler(signUp));
 
-router.post('/login', authLimiter, (req, res, next) => {
-    login(req, res).catch(next);
-});
+router.post('/login', authLimiter, asyncHandler(login));
 
-router.post('/refresh-token', (req, res, next) => {
-  refreshAccessToken(req, res).catch(next);
-});
+router.post('/refresh-token', asyncHandler(refreshAccessToken));
 
-router.post('/logout', authenticateToken, (req, res, next) => {
-  logout(req, res).catch(next);
-});
+router.post('/logout', authenticateToken, asyncHandler(logout));
 
 export default router;
