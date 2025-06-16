e682db5939a2474e02af9db99acb6e1f32479d0e
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-06-15 23:38:29 -0300

    criado o utils async handler

---
+++ src/utils/async-handler.ts
@@ -0,0 +1,11 @@
+import { RequestHandler } from 'express';
+
+export const asyncHandler = <P, ResBody, ReqBody, ReqQuery>(
+  fn: (req: import('express').Request<P, ResBody, ReqBody, ReqQuery>, 
+       res: import('express').Response<ResBody>, 
+       next: import('express').NextFunction) => Promise<any>
+): RequestHandler<P, ResBody, ReqBody, ReqQuery> => {
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
