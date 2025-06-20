8759b64720481c66244eebdcf1c92f143e233dd7
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-06-20 13:10:25 -0300

    swagger sendo chamado no server

--- src/server.ts
+++ src/server.ts
@@ -8,29 +8,31 @@ import userDataRouter from './modules/user/user.routes';
 import storeDataRoutes from './modules/store/store.routes';
 import uploadFile from './modules/uploads/upload.routes';
 import updateSchedule from './modules/schedules/schedules.routes';
-import menuCategoriesRoutes from './modules/manage-menu/menu-category/menu-category.routes'
-import menuItemRoutes from './modules/manage-menu/menu-items/menu-item.routes'
-import ordersRoutes from './modules/create-order/by-store/order.routes'
-import manageOrdersRoutes from './modules/manage-order/by-store/orders.routes'
-import pageStyle from './modules/menu-customization/store-customization.routes'
-import './modules/password/clean-expired-tokens.utils'
+import menuCategoriesRoutes from './modules/manage-menu/menu-category/menu-category.routes';
+import menuItemRoutes from './modules/manage-menu/menu-items/menu-item.routes';
+import ordersRoutes from './modules/create-order/by-store/order.routes';
+import manageOrdersRoutes from './modules/manage-order/by-store/orders.routes';
+import pageStyle from './modules/menu-customization/store-customization.routes';
+import './modules/password/clean-expired-tokens.utils';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 
     {/*ANTES DE COLOCAR O PROJETO EM PRODUÇÃO FAZER TESTES SALVANDO DADOS NO CACHE
     PRA DIMINUIR O NUMERO DE CONSULTAR NO BD E DIMINUIR A LATÊNCIA */}
 
 const app = express();
 const port = process.env.PORT || 3000;
+const swaggerDocument = YAML.load(path.resolve(__dirname, './docs/swagger.yaml'));
 
 app.set('trust proxy', 1);
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-}));
+app.use(cors());
 
 app.use('/', authRoutes);
 app.use('/', resetPasswordRoutes);
