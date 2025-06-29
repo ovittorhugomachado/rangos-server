7a73ee1a6358e1c4f9a6bda0a52f9b28e5ec5b04
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-06-29 11:31:46 -0300

    token durando 1 dia

--- src/modules/auth/generate-tokens.ts
+++ src/modules/auth/generate-tokens.ts
@@ -5,7 +5,7 @@ const REFRESH_SECRET = process.env.REFRESH_SECRET || 'refresh_secreto';
 
 export function generateTokens(payload: object) {
 
-  const accessToken = jwt.sign(payload, JWT_SECRET, { expiresIn: '15m' });
+  const accessToken = jwt.sign(payload, JWT_SECRET, { expiresIn: '1d' });
   
   const refreshToken = jwt.sign(payload, REFRESH_SECRET, { expiresIn: '30d' });
 
