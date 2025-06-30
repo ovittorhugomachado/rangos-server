f8ca8e8a188486ef82a57168dea5cc73697e50a0
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-06-30 10:57:16 -0300

    deletado estilo cor de texto

---
+++ prisma/migrations/20250630132932_coluna_text_color_deletada/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `textColor` on the `store_styles` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "store_styles" DROP COLUMN "textColor";
